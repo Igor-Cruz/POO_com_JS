5d0187c6bf058d51c12fcc96b1fd9148663addd7
IGOR CRUZ <igorcruz.it@outlook.com>
2022-04-28 20:24:28 -0300

    Criando o objeto

---
+++ 1_criando_objetos/script.js
@@ -0,0 +1,14 @@
+//Primeiro passo Definir uma variável
+let carro = {
+  //Propriedades
+  portas: 4,
+  cor: 'branco'
+
+}
+
+console.log(carro)
+
+//Tipos de acesso ao objeto
+
+console.log(carro.portas)
+console.log(carro[cor])
